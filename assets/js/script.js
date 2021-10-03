f53d4258807f2dbf719b5676792bf0adb15a0c66
Leithen <leithen113@gmail.com>
2021-10-03 11:25:37 -0600

    Added moment to handle date and time

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  //check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -56,6 +58,28 @@ $(".list-group").on("click", "p", function() {
   textInput.trigger("focus");
 });
 
+var auditTask = function(taskEl) {
+  //get date from task element
+  var date = $(taskEl)
+    .find("span")
+    .text()
+    .trim();
+  console.log(date);
+
+  //convert to moment object @ 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+
+  //remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  //apply new class if task is near or over due
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
+
 //when not in focus, save textarea content as p (SAVE TASK DESCRIPTION)
 $(".list-group").on("blur", "textarea", function() {
   //get the textarea's current value
@@ -93,14 +117,23 @@ $(".list-group").on("click", "span", function() {
     .attr("type", "text")
     .addClass("form-control")
     .val(date);
-  //swap out elements
+
   $(this).replaceWith(dateInput);
+
+  //enable jQuery UI datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      $(this).trigger("change");
+    }
+  });
+
   //automatically focus on new element
   dateInput.trigger("focus");
 });
 
 //when not in focus, save input content as span (SAVE DUE DATE)
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   //get the input's current value
   var date = $(this)
   .val()
@@ -121,8 +154,10 @@ $(".list-group").on("blur", "input[type='text']", function() {
   var taskSpan = $("<span>")
     .addClass("badge badge-primary badge-pill")
     .text(date);
-    //replace input with span
+  //replace input with span
   $(this).replaceWith(taskSpan);
+  //pass task's ,li. element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"))
 });
 
 $(".card .list-group").sortable({
@@ -220,6 +255,10 @@ $("#task-form-modal .btn-primary").click(function() {
 
     saveTasks();
   }
+});
+
+$("#modalDueDate").datepicker({
+  minDate: 1
 });
 
 // remove all tasks
